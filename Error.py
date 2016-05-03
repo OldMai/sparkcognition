519d07fef5bf694d41de2b16ce5a3949ce3eeebf
Carishma <shetty.carishma@gmail.com>
2016-05-02 20:29:42 -0500

    Update Error.py

--- Error.py
+++ Error.py
@@ -6,7 +6,7 @@
 import pandas as pd
 import numpy as np
 import glob
-
+# reading files with actual consumption for each home ID
 files = glob.glob('test/*.csv')
 print files
 k=' '
@@ -15,20 +15,26 @@ for j in files:
     if j.endswith('__weekday_out.csv'):
         k = j[5:-17]
     	frame1=pd.read_csv(j)
+    	# reading files with predicted data for each home ID
     	frame2=pd.read_csv('solution_'+str(k)+'.csv')
+    	# Appliances for which consumption is predicted
     	frame2.columns=['Air predicted','Dryer predicted','Furnace predicted','Light plugs predicted','Refrigerator predicted']
     	frame2['Timestamp']=frame1['Timestamp']
+    	#Merging actual and predicted consumption into a data frame
     	df2=pd.merge(frame1,frame2)
 #    	print(df2)
 #    	df=df2.drop(df2.columns[[0,6,7,8]],axis=1,inplace=True)
+	# Dropping  header
     	df=df2.drop(['Unnamed: 0','Timestamp','Total','weekday'], axis=1)
 #    	print(df)
+	# Initializing list for each error type eg. root mean square error, mean absolute percentage error, etc.
     	errormp=[]
     	mse=[]
     	rmse=[]
     	norm_error=[]
     	dev=[]
         mape=[]
+        # Appliances for which prediction error is calculated
     	appliance=['Air','Dryer', 'Furnace','Light plugs','Refrigerator']
    	# for i in range(len(df.columns)/2):
     	for words in appliance:
